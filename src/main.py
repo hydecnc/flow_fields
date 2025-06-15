a582ee239929ddee88966a9a2d3aafe05c6dae6f
Hyde Yoo <hydeyoo0716@gmail.com>
2025-06-15 18:03:27 +0900

    feat: add progress bar

--- src/main.py
+++ src/main.py
@@ -1,13 +1,14 @@
 import math
 import cairo
 
-from arrow import Arrow
 from particle import Particle
-from perlin import fractal_brownian_motion, lerp
+from perlin import Perlin2D, lerp
 from vector import Vec2D, Vec3D
 
-WIDTH, HEIGHT = 1000, 1000
-BACKGROUND_COLOR = Vec3D(1.0, 1.0, 1.0)
+from tqdm import tqdm
+
+WIDTH, HEIGHT = 1600, 1600
+BACKGROUND_COLOR = Vec3D(0.0, 0.0, 0.0)
 PARTICLE_RADIUS = 0.002
 NUM_COLS = 100
 NUM_ROWS = 100
@@ -20,15 +21,13 @@ def draw_curve(
     step_size: float = 0.003,
     width: float = 0.002,
     color: Vec3D = Vec3D(0, 1, 0),
-):
-    # draw curve
+) -> None:
     curve = start_point
     ctx.set_source_rgb(color.r, color.g, color.b)
     ctx.set_line_width(width)
     ctx.move_to(curve.x, curve.y)
 
     for _ in range(num_steps):
-        # print(curve)
         column_index = int(curve.x * NUM_COLS)
         row_index = int(curve.y * NUM_ROWS)
 
@@ -48,17 +47,21 @@ def draw_curve(
 
 
 if __name__ == "__main__":
+    # Initial setup
     surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, WIDTH, HEIGHT)
     ctx = cairo.Context(surface)
 
     ctx.scale(WIDTH, HEIGHT)
 
-    # set background
+    # Set background
     ctx.rectangle(0, 0, 1, 1)
     ctx.set_source_rgb(BACKGROUND_COLOR.r, BACKGROUND_COLOR.g, BACKGROUND_COLOR.b)
     ctx.fill()
 
-    # add particles to grid
+    # Setup noise
+    perlin_2d = Perlin2D()
+
+    # Add particles to grid
     grid = []
     row_step = 1 / NUM_ROWS
     col_step = 1 / NUM_COLS
@@ -66,33 +69,24 @@ if __name__ == "__main__":
     for row in range(NUM_ROWS + 1):
         grid_row = []
         for col in range(NUM_COLS + 1):
-            angle = lerp(fractal_brownian_motion(row, col, NUM_OCTAVES), 0, 2 * math.pi)
+            angle = lerp(
+                perlin_2d.fractal_brownian_motion(row, col, NUM_OCTAVES), 0, 2 * math.pi
+            )
             grid_row.append(Particle(pos.x, pos.y, angle))
             pos.x += col_step
         grid.append(grid_row)
         pos.x = 0
         pos.y += row_step
 
-    # draw particles
+    # Draw particles
     ctx.set_source_rgb(0, 0, 0)
-    for row in grid:
-        for particle in row:
-            # draw the arrow
-            step = (
-                Vec2D(
-                    math.cos(particle.angle),
-                    math.sin(particle.angle),
-                )
-                / 100
-            )
-            # arrow = Arrow(particle.pos(), particle.pos() + step, 0.005)
-            # arrow.draw(ctx)
-
+    for row in tqdm(grid, desc="Rows"):
+        for particle in tqdm(row, desc="Particles", leave=False):
             # draw a curve at each position of the particle.
             draw_curve(
                 particle.pos(),
                 200,
-                color=Vec3D(particle.y, particle.x, particle.x * particle.y),
+                color=Vec3D(particle.y, particle.x, particle.x),
                 width=0.001,
                 # step_size=0.001,
             )
